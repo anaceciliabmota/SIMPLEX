a9cf06d4e774bec8534ea9b80666e837243db09e
Ana Cecília <anaceciliabezerra10@gmail.com>
2024-06-03 21:09:45 -0300

    Adição da função de reducedCost e inicio da implementação do algoritmo do simplex revisado

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 #include <algorithm>
+#include <limits>
 #include <Eigen/Dense>
 
 using namespace std;
 using namespace Eigen;
+
+//colocar o simplex como uma funcao
+//fazer função para calculo de z 
+//colocar exceção de u < 0
+
+struct Solution {
+    bool isOptimal;
+    double z;
+    MatrixXd variaveis_basicas;
+} typedef Solution;
 
 void loadB(MatrixXd& B, VectorXd& u, int l)
 {
@@ -37,13 +48,14 @@ void findC(VectorXd& c, double fo[], MatrixXd& vb, int n)
 void calculateP(VectorXd& c, MatrixXd& B, VectorXd& p)
 {
     p = c.transpose() * B; //por padrao, c é coluna, então a transposicao é para transformá-lo em uma linha
-    cout << p << endl;
+    //cout << p << endl;
 }
 
 // A[j] é uma coluna e nao uma linha
 void calculateU(MatrixXd &B, MatrixXd& A, VectorXd& u, int j)
 {
     u = B * A.col(j);
+    //!any u > 0, nao tem solução otima
 }
 
 void calculateReducedC(double fo[], VectorXd& reduced_cost, MatrixXd& A, VectorXd& p, MatrixXd& vb){
@@ -54,12 +66,12 @@ void calculateReducedC(double fo[], VectorXd& reduced_cost, MatrixXd& A, VectorX
             reduced_cost(i) = fo[i] - p.transpose() * A.col(i);
         }
     }
-    cout << reduced_cost << endl;
+    //cout << reduced_cost << endl;
 }
 
 bool isOptimal(VectorXd& reduced_cost){
     for(int i = 0; i < reduced_cost.rows(); i++){
-        if(reduced_cost(i) > 0){
+        if(reduced_cost(i) < 0){
             return false;
         }
     }
@@ -67,34 +79,63 @@ bool isOptimal(VectorXd& reduced_cost){
     //para escolher j: reduced_cost.maxCoeff();
 }
 
+int findTeta(MatrixXd& vb, VectorXd u, double * teta){
+    double min = numeric_limits<double>::infinity();
+    int l;
+    for(int i = 0; i < vb.rows();i++){
+        if(u(i) > 0 && min > vb(i, 1)/u(i)){
+            min = vb(i,1)/u(i);
+            l = i; //ou l = vb(i, 0)?
+        }
+    }
+    *teta = min;
+    return l;
+}
+int chooseJ(VectorXd& reduced_cost){
+    Index index;
+    reduced_cost.minCoeff(&index);
+    return static_cast<int>(index);
+}
 
+void changingVariables(MatrixXd& vb, VectorXd& u, double teta, int l, int j){
+    for(int i = 0; i < vb.rows(); i++){
+        if(i == l){
+            vb(i, 0) = j;
+            vb(i, 1) = teta;
+        }else{
+            vb(i, 1) = vb(i, 1) - teta*u(i);
+        }
+    }
+}
 int main()
 {
     ////passando dados manualmente enquanto nao tenho a leitura de arquivos/////
+    int n;
     MatrixXd A(3, 7);
+    n = A.rows();
     A << 3, 2, 1, 2, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 4, 3, 3, 4, 0, 0, 1;
-    MatrixXd B(3, 3);
+    MatrixXd B(n, n);
     B << 3, 1, 0, 1, 1, 0, 4, 3, 1;
     
-    double fo[] = {19, 13, 12, 17, 0, 0, 0}; // função objetivo
-    VectorXd rhs(3);
+    double fo[] = {-19, -13, -12, -17, 0, 0, 0}; // função objetivo
+    VectorXd rhs(n);
     rhs << 225, 117, 420;
 
-    MatrixXd variaveis_basicas(3, 2);
+    MatrixXd variaveis_basicas(n, 2);
     variaveis_basicas.col(0) << 0, 2, 6;  //linha 1 da matriz serão os indices da matriz -> variavel -1
     variaveis_basicas.col(1) = B.inverse() * rhs;
     
-    
+    //cout << variaveis_basicas << endl;
     // calculando a inversa
     B = B.inverse();
     cout << "Matrix B inversa \n"
          << B << endl;
 
-    VectorXd c(B.cols()); // c = coeficientes da fo das variaveis basicas
-    VectorXd p(B.cols()); // p = duais
-    VectorXd u(B.cols()); // u = B^-1*Aj
+    VectorXd c(n); // c = coeficientes da fo das variaveis basicas
+    VectorXd p(n); // p = duais
+    VectorXd u(n); // u = B^-1*Aj
     VectorXd reduced_cost(A.cols());
-    
+    /*
     findC(c, fo, variaveis_basicas, A.rows());
     calculateP(c, B, p);
     calculateReducedC(fo, reduced_cost, A, p, variaveis_basicas);
@@ -108,6 +149,38 @@ int main()
     loadB(teste, u, 2);
     cout << "Here is the input matrix teste \n"
          << teste << endl;
+    */
+    Solution s;
+    while(true){
+        findC(c, fo, variaveis_basicas, A.rows());
+        calculateP(c, B, p);
+        //cout << "P:" << p << endl;
+        calculateReducedC(fo, reduced_cost, A, p, variaveis_basicas);
+        //cout << "rc: " << reduced_cost << endl;
+        if(isOptimal(reduced_cost)){
+            s.variaveis_basicas = variaveis_basicas;
+            break;
+            //fazer função de calcular z
+            //passar valores para as variaveis de solução
+            //break
+        }
+        else{
+            int j = chooseJ(reduced_cost);
+            //cout << "j: " << j << endl;
+            calculateU(B, A, u , j);
+            //cout << "u:" << u << endl;
+            double teta;
+            int l = findTeta(variaveis_basicas, u, &teta);
+            //cout << "l: " <<  l << " teta: " << teta << endl;
+            changingVariables(variaveis_basicas, u, teta, l, j);
+            //cout << "vb: " << endl << variaveis_basicas << endl;
+            loadB(B, u, l);
+            //cout << endl << "novo B" ;
+            //cout << B << endl;
+        }
+    }
+    cout << "Solucao:" << endl; 
+    cout << variaveis_basicas << endl;
     
     return 0;
 }
