9aada948cdf397d36acc9e0894e9ed35efcaeb8b
Ana Cecília <anaceciliabezerra10@gmail.com>
2024-06-04 11:16:13 -0300

    organização do codigo e adição do calculo de Z

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,6 @@
 using namespace std;
 using namespace Eigen;
 
-//colocar o simplex como uma funcao
-//fazer função para calculo de z 
 //colocar exceção de u < 0
 
 struct Solution {
@@ -61,7 +59,7 @@ void calculateU(MatrixXd &B, MatrixXd& A, VectorXd& u, int j)
 void calculateReducedC(double fo[], VectorXd& reduced_cost, MatrixXd& A, VectorXd& p, MatrixXd& vb){
     reduced_cost.setZero();
     for(int i = 0; i < A.cols(); i++){
-        if(!(vb.array() == i).any()){
+        if(!(vb.col(0).array() == i).any()){
             //significa que é nao basica
             reduced_cost(i) = fo[i] - p.transpose() * A.col(i);
         }
@@ -107,6 +105,44 @@ void changingVariables(MatrixXd& vb, VectorXd& u, double teta, int l, int j){
         }
     }
 }
+
+Solution SIMPLEX(MatrixXd& A, MatrixXd& B, double fo[], MatrixXd& variaveis_basicas, int n){
+    B = B.inverse();
+
+    VectorXd c(n); // c = coeficientes da fo das variaveis basicas
+    VectorXd p(n); // p = duais
+    VectorXd u(n); // u = B^-1*Aj
+    VectorXd reduced_cost(A.cols());
+    
+    Solution s;
+
+    while(true){
+        findC(c, fo, variaveis_basicas, A.rows());
+        calculateP(c, B, p);
+        
+        calculateReducedC(fo, reduced_cost, A, p, variaveis_basicas);
+        
+        if(isOptimal(reduced_cost)){
+            s.variaveis_basicas = variaveis_basicas;
+            s.z = c.transpose() * variaveis_basicas.col(1);
+            break;
+        }
+        else{
+            int j = chooseJ(reduced_cost);
+            calculateU(B, A, u , j);
+            if(u.maxCoeff() <= 0){
+                s.z = -1*numeric_limits<double>::infinity();
+                break;
+            }else{
+                double teta;
+                int l = findTeta(variaveis_basicas, u, &teta);
+                changingVariables(variaveis_basicas, u, teta, l, j);
+                loadB(B, u, l);
+            }
+        }
+    }
+    return s;
+}
 int main()
 {
     ////passando dados manualmente enquanto nao tenho a leitura de arquivos/////
@@ -124,63 +160,33 @@ int main()
     MatrixXd variaveis_basicas(n, 2);
     variaveis_basicas.col(0) << 0, 2, 6;  //linha 1 da matriz serão os indices da matriz -> variavel -1
     variaveis_basicas.col(1) = B.inverse() * rhs;
-    
-    //cout << variaveis_basicas << endl;
-    // calculando a inversa
-    B = B.inverse();
-    cout << "Matrix B inversa \n"
-         << B << endl;
 
-    VectorXd c(n); // c = coeficientes da fo das variaveis basicas
-    VectorXd p(n); // p = duais
-    VectorXd u(n); // u = B^-1*Aj
-    VectorXd reduced_cost(A.cols());
-    /*
-    findC(c, fo, variaveis_basicas, A.rows());
-    calculateP(c, B, p);
-    calculateReducedC(fo, reduced_cost, A, p, variaveis_basicas);
-    
-    // matriz e u para teste de loadB (exemplo livro bertsimas)
-    MatrixXd teste(3, 3);
-    teste << 1, 2, 3, -2, 3, 1, 4, -3, -2;
-    cout << "Here is the input matrix teste \n"
-         << teste << endl;
-    u << -4, 2, 2;
-    loadB(teste, u, 2);
-    cout << "Here is the input matrix teste \n"
-         << teste << endl;
-    */
-    Solution s;
-    while(true){
-        findC(c, fo, variaveis_basicas, A.rows());
-        calculateP(c, B, p);
-        //cout << "P:" << p << endl;
-        calculateReducedC(fo, reduced_cost, A, p, variaveis_basicas);
-        //cout << "rc: " << reduced_cost << endl;
-        if(isOptimal(reduced_cost)){
-            s.variaveis_basicas = variaveis_basicas;
-            break;
-            //fazer função de calcular z
-            //passar valores para as variaveis de solução
-            //break
+
+    Solution s = SIMPLEX(A, B, fo, variaveis_basicas, n);
+    if(s.z != -1*numeric_limits<double>::infinity()){
+        cout << "Solucao:" << endl; 
+        for(int i = 0; i < n; i++){
+            cout << "x" << variaveis_basicas(i, 0) + 1 << ": " << s.variaveis_basicas(i, 1) << endl;
         }
-        else{
-            int j = chooseJ(reduced_cost);
-            //cout << "j: " << j << endl;
-            calculateU(B, A, u , j);
-            //cout << "u:" << u << endl;
-            double teta;
-            int l = findTeta(variaveis_basicas, u, &teta);
-            //cout << "l: " <<  l << " teta: " << teta << endl;
-            changingVariables(variaveis_basicas, u, teta, l, j);
-            //cout << "vb: " << endl << variaveis_basicas << endl;
-            loadB(B, u, l);
-            //cout << endl << "novo B" ;
-            //cout << B << endl;
-        }
+        cout << "z: " << s.z << endl;
+    }else{
+        cout << "Solução ótima é igual a menos infinito";
     }
-    cout << "Solucao:" << endl; 
-    cout << variaveis_basicas << endl;
+    
     
     return 0;
 }
+
+/*MatrixXd A(, );
+    n = A.rows();
+    A << ;
+    MatrixXd B(n, n);
+    B << ;
+    
+    double fo[] = {-}; // função objetivo
+    VectorXd rhs(n);
+    rhs << 2;
+
+    MatrixXd variaveis_basicas(n, 2);
+    variaveis_basicas.col(0) << ;  //linha 1 da matriz serão os indices da matriz -> variavel -1
+    variaveis_basicas.col(1) = B.inverse() * rhs;*/
